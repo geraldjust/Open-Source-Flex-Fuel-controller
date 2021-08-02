9f973d7b46aa6bb7ffa3ed364a0ffe01fa2e5459
geraldjust <josegerardo12345@gmail.com>
2021-08-01 18:52:09 -0700

    Update F0_Flex_fuel_kit.ino
    we will separate to different INO files

--- F0_Flex_fuel_kit/F0_Flex_fuel_kit.ino
+++ F0_Flex_fuel_kit/F0_Flex_fuel_kit.ino
@@ -123,72 +123,10 @@ static void checkFailsafeValues(void)
   isError = false;
 }
 
-/************************************************
- * Interrupt services
- ************************************************/
-void SETUP_ISR() {
-  pinMode(PIN_SENSOR_IN, INPUT);
-  attachInterrupt(SEN_IN, READ_Frequency, RISING);
-  attachInterrupt(SEN_IN, READ_DutyCycle, FALLING);
-  start_Hz_MS = micros();
-  start_Duty_MS = micros();
-}
 
-//rise to rise to check for Hz
-void READ_Frequency() {
-  elapsed_HZ_MS = micros() - start_Hz_MS;
-  start_Hz_MS = micros();
-  start_Duty_MS = micros();//for temp rising edge
-}
 
-//rise starts in thr freq cycle and ends in a falling
-void READ_DutyCycle() {
-  elapsed_Duty_MS = micros() - start_Duty_MS;//falling edge
-}
 
-/***********************************************
- * Debug
- ***********************************************/
-#ifdef DEBUG
-#define SERIAL_BAUD    115200
 
-static void debugPrint(void)
-{
-  Serial.print("Hrtz: " + freq );
-  Serial.print("  ethanol%: " + ethanol) ;
-  Serial.print("  ethenol ms: " + elapsed_HZ_MS);
-  Serial.print("  Duty Cycle ms:");
-  Serial.print(D_cycle);
-  Serial.print("  Temperature in c:"  );
-  Serial.println(Temperature_C);
-}
-
-static void initAndRegisterSerialTerminalDebug()
-{
-  Serial.begin(SERIAL_BAUD);
-  flexFuelServiceFnTbl[FF_HDLR_SERIAL_PRINT_DIAG] = debugPrint;
-
-}
-#endif
-
-/************************************************
- * Initialization functions
- ************************************************/
-void registerServiceFns(void)
-{
-  /* ESSENTIAL FEATURES: DO NOT COMMENT ANY OF THESE OUT UNLESS YOU WANT YOUR CAR COOKED */
-  flexFuelServiceFnTbl[FF_HDLR_CALC_FREQ_DCYCLE] = calcFreqAndDutyCycle;
-  flexFuelServiceFnTbl[FF_HDLR_CALC_ETH_CONTENT] = calcEthContent;
-
-  /* Optional features: Initialize if desired, comment out if not needed. */
-  flexFuelServiceFnTbl[FF_HDLR_CALC_FUEL_TEMP]   = calcFuelTemp;
-  flexFuelServiceFnTbl[FF_HDLR_FAILSAFE]   = checkFailsafeValues;
-
-  /* Debug features: Only used for debugging. */
-  #ifdef DEBUG
-  initAndRegisterSerialTerminalDebug();
-  #endif
-}
 
 void setup(void)
 {
@@ -210,4 +148,3 @@ void loop()
     }
   }
 }
-
