325f2c8ee6932f1c057146c7e8f4f80c37d97e96
geraldjust <josegerardo12345@gmail.com>
2021-08-25 01:47:46 -0700

    added canbus
    very tough implementation of can bus.

--- F0_Flex_fuel_kit/F0_Flex_fuel_kit.ino
+++ F0_Flex_fuel_kit/F0_Flex_fuel_kit.ino
@@ -2,8 +2,376 @@
 #include "defines.h"
 /* #include "flex_fuel_def.h" */ /* TODO: makefile should include libraries based on platform/board. */
 
+
+
+//TODO: clean up all can stuff from line 13 though 364...
+#define DEBUG 1
+#define AF4   0x04
+#define AF0   0x00
+
+/* Symbolic names for bit rate of CAN message                                */
+typedef enum {CAN_50KBPS, CAN_100KBPS, CAN_125KBPS, CAN_250KBPS, CAN_500KBPS, CAN_1000KBPS} BITRATE;
+
+/* Symbolic names for formats of CAN message                                 */
+typedef enum {STANDARD_FORMAT = 0, EXTENDED_FORMAT} CAN_FORMAT;
+
+/* Symbolic names for type of CAN message                                    */
+typedef enum {DATA_FRAME = 0, REMOTE_FRAME}         CAN_FRAME;
+
+typedef struct
+{
+  uint32_t id;        /* 29 bit identifier                               */
+  uint8_t  data[8];   /* Data field                                      */
+  uint8_t  len;       /* Length of data field in bytes                   */
+  uint8_t  ch;        /* Object channel(Not use)                         */
+  uint8_t  format;    /* 0 - STANDARD, 1- EXTENDED IDENTIFIER            */
+  uint8_t  type;      /* 0 - DATA FRAME, 1 - REMOTE FRAME                */
+} CAN_msg_t;
+
+typedef const struct
+{
+  uint8_t TS2;
+  uint8_t TS1;
+  uint8_t BRP;
+} CAN_bit_timing_config_t;
+
+CAN_bit_timing_config_t can_configs[6] = {{2, 13, 60}, {2, 13, 30}, {2, 13, 24}, {2, 13, 12}, {2, 13, 6}, {2, 13, 3}};
+
+/**
+   Print registers.
+*/
+void printRegister(char * buf, uint32_t reg) {
+  if (DEBUG == 0) return;
+  Serial.print(buf);
+  Serial.print("0x");
+  Serial.print(reg, HEX);
+  Serial.println();
+}
+
+/**
+   Initializes the CAN GPIO registers.
+
+   @params: addr    - Specified GPIO register address.
+   @params: index   - Specified GPIO index.
+   @params: afry    - Specified Alternative function selection AF0-AF15.
+   @params: speed   - Specified OSPEEDR register value.(Optional)
+
+*/
+void CANSetGpio(GPIO_TypeDef * addr, uint8_t index, uint8_t afry, uint8_t speed = 3) {
+  uint8_t _index2 = index * 2;
+  uint8_t _index4 = index * 4;
+  uint8_t ofs = 0;
+  uint8_t setting;
+
+  if (index > 7) {
+    _index4 = (index - 8) * 4;
+    ofs = 1;
+  }
+
+  uint32_t mask;
+  printRegister("GPIO_AFR(b)=", addr->AFR[1]);
+  mask = 0xF << _index4;
+  addr->AFR[ofs]  &= ~mask;         // Reset alternate function
+  //setting = 0x9;                    // AF9
+  setting = afry;                   // Alternative function selection
+  mask = setting << _index4;
+  addr->AFR[ofs]  |= mask;          // Set alternate function
+  printRegister("GPIO_AFR(a)=", addr->AFR[1]);
+
+  printRegister("GPIO_MODER(b)=", addr->MODER);
+  mask = 0x3 << _index2;
+  addr->MODER   &= ~mask;           // Reset mode
+  setting = 0x2;                    // Alternate function mode
+  mask = setting << _index2;
+  addr->MODER   |= mask;            // Set mode
+  printRegister("GPIO_MODER(a)=", addr->MODER);
+
+  printRegister("GPIO_OSPEEDR(b)=", addr->OSPEEDR);
+  mask = 0x3 << _index2;
+  addr->OSPEEDR &= ~mask;           // Reset speed
+  setting = speed;
+  mask = setting << _index2;
+  addr->OSPEEDR |= mask;            // Set speed
+  printRegister("GPIO_OSPEEDR(a)=", addr->OSPEEDR);
+
+  printRegister("GPIO_OTYPER(b)=", addr->OTYPER);
+  mask = 0x1 << index;
+  addr->OTYPER  &= ~mask;           // Reset Output push-pull
+  printRegister("GPIO_OTYPER(a)=", addr->OTYPER);
+
+  printRegister("GPIO_PUPDR(b)=", addr->PUPDR);
+  mask = 0x3 << _index2;
+  addr->PUPDR   &= ~mask;           // Reset port pull-up/pull-down
+  printRegister("GPIO_PUPDR(a)=", addr->PUPDR);
+}
+
+
+/**
+   Initializes the CAN filter registers.
+
+   @preconditions   - This register can be written only when the filter initialization mode is set (FINIT=1) in the CAN_FMR register.
+   @params: index   - Specified filter index. index 27:14 are available in connectivity line devices only.
+   @params: scale   - Select filter scale.
+                      0: Dual 16-bit scale configuration
+                      1: Single 32-bit scale configuration
+   @params: mode    - Select filter mode.
+                      0: Two 32-bit registers of filter bank x are in Identifier Mask mode
+                      1: Two 32-bit registers of filter bank x are in Identifier List mode
+   @params: fifo    - Select filter assigned.
+                      0: Filter assigned to FIFO 0
+                      1: Filter assigned to FIFO 1
+   @params: bank1   - Filter bank register 1
+   @params: bank2   - Filter bank register 2
+
+*/
+void CANSetFilter(uint8_t index, uint8_t scale, uint8_t mode, uint8_t fifo, uint32_t bank1, uint32_t bank2) {
+  if (index > 27) return;
+
+  CAN->FA1R &= ~(0x1UL << index);             // Deactivate filter
+
+  if (scale == 0) {
+    CAN->FS1R &= ~(0x1UL << index);           // Set filter to Dual 16-bit scale configuration
+  } else {
+    CAN->FS1R |= (0x1UL << index);            // Set filter to single 32 bit configuration
+  }
+  if (mode == 0) {
+    CAN->FM1R &= ~(0x1UL << index);           // Set filter to Mask mode
+  } else {
+    CAN->FM1R |= (0x1UL << index);            // Set filter to List mode
+  }
+
+  if (fifo == 0) {
+    CAN->FFA1R &= ~(0x1UL << index);          // Set filter assigned to FIFO 0
+  } else {
+    CAN->FFA1R |= (0x1UL << index);           // Set filter assigned to FIFO 1
+  }
+
+  CAN->sFilterRegister[index].FR1 = bank1;    // Set filter bank registers1
+  CAN->sFilterRegister[index].FR2 = bank2;    // Set filter bank registers2
+
+  CAN->FA1R |= (0x1UL << index);              // Activate filter
+
+}
+
+
+/**
+   Initializes the CAN controller with specified bit rate.
+
+   @params: bitrate - Specified bitrate. If this value is not one of the defined constants, bit rate will be defaulted to 125KBS
+   @params: remap   - Select CAN port.
+                      =0:CAN_RX mapped to PA11, CAN_TX mapped to PA12
+                      =1:Not used
+                      =2:CAN_RX mapped to PB8, CAN_TX mapped to PB9 (not available on 36-pin package)
+                      =3:CAN_RX mapped to PD0, CAN_TX mapped to PD1 (available on 100-pin and 144-pin package)
+
+*/
+bool CANInit(BITRATE bitrate, int remap)
+{
+  // Reference manual
+  // https://www.st.com/resource/en/reference_manual/dm00031936-stm32f0x1stm32f0x2stm32f0x8-advanced-armbased-32bit-mcus-stmicroelectronics.pdf
+
+  RCC->APB1ENR |= 0x2000000UL;          // Enable CAN clock
+
+  if (remap == 0) {
+    RCC->AHBENR |= 0x20000UL;           // Enable GPIOA clock
+    CANSetGpio(GPIOA, 11, AF4);         // Set PA11 to AF4
+    CANSetGpio(GPIOA, 12, AF4);         // Set PA12 to AF4
+  }
+
+  if (remap == 2) {
+    RCC->AHBENR |= 0x40000UL;           // Enable GPIOB clock
+    CANSetGpio(GPIOB, 8, AF4);          // Set PB8 to AF4
+    CANSetGpio(GPIOB, 9, AF4);          // Set PB9 to AF4
+  }
+
+  if (remap == 3) {
+    RCC->AHBENR |= 0x100000UL;          // Enable GPIOD clock
+    CANSetGpio(GPIOD, 0, AF0);          // Set PD0 to AF0
+    CANSetGpio(GPIOD, 1, AF0);          // Set PD1 to AF0
+  }
+
+  CAN->MCR |= 0x1UL;                   // Set CAN to Initialization mode
+  while (!(CAN->MSR & 0x1UL));         // Wait for Initialization mode
+
+  //CAN->MCR = 0x51UL;                 // Hardware initialization(No automatic retransmission)
+  CAN->MCR = 0x41UL;                   // Hardware initialization(With automatic retransmission)
+
+  // Set bit rates
+  //CAN1->BTR &= ~(((0x03) << 24) | ((0x07) << 20) | ((0x0F) << 16) | (0x1FF));
+  //CAN1->BTR |=  (((can_configs[bitrate].TS2-1) & 0x07) << 20) | (((can_configs[bitrate].TS1-1) & 0x0F) << 16) | ((can_configs[bitrate].BRP-1) & 0x1FF);
+  CAN->BTR &= ~(((0x03) << 24) | ((0x07) << 20) | ((0x0F) << 16) | (0x3FF));
+  CAN->BTR |=  (((can_configs[bitrate].TS2 - 1) & 0x07) << 20) | (((can_configs[bitrate].TS1 - 1) & 0x0F) << 16) | ((can_configs[bitrate].BRP - 1) & 0x3FF);
+  printRegister("CAN->BTR=", CAN->BTR);
+
+  // Configure Filters to default values
+  CAN->FMR  |=   0x1UL;                // Set to filter initialization mode
+  CAN->FMR  &= 0xFFFFC0FF;             // Clear CAN2 start bank
+
+  // bxCAN has 28 filters.
+  // These filters are used for both CAN1 and CAN2.
+  // STM32F072 has only CAN1, so all 28 are used for CAN1
+  CAN->FMR  |= 0x1C << 8;              // Assign all filters to CAN1
+
+  // Set fileter 0
+  // Single 32-bit scale configuration
+  // Two 32-bit registers of filter bank x are in Identifier Mask mode
+  // Filter assigned to FIFO 0
+  // Filter bank register to all 0
+  CANSetFilter(0, 1, 0, 0, 0x0UL, 0x0UL);
+
+  CAN->FMR   &= ~(0x1UL);              // Deactivate initialization mode
+
+  uint16_t TimeoutMilliseconds = 100;
+  bool can1 = false;
+  CAN->MCR   &= ~(0x1UL);              // Require CAN1 to normal mode
+
+  // Wait for normal mode
+  // If the connection is not correct, it will not return to normal mode.
+  for (uint16_t wait_ack = 0; wait_ack < TimeoutMilliseconds; wait_ack++) {
+    if ((CAN->MSR & 0x1UL) == 0) {
+      can1 = true;
+      break;
+    }
+    delayMicroseconds(100);
+  }
+  //Serial.print("can1=");
+  //Serial.println(can1);
+  if (can1) {
+    Serial.println("CAN1 initialize ok");
+  } else {
+    Serial.println("CAN1 initialize fail!!");
+    return false;
+  }
+  return true;
+}
+
+
+#define STM32_CAN_TIR_TXRQ              (1U << 0U)  // Bit 0: Transmit Mailbox Request
+#define STM32_CAN_RIR_RTR               (1U << 1U)  // Bit 1: Remote Transmission Request
+#define STM32_CAN_RIR_IDE               (1U << 2U)  // Bit 2: Identifier Extension
+#define STM32_CAN_TIR_RTR               (1U << 1U)  // Bit 1: Remote Transmission Request
+#define STM32_CAN_TIR_IDE               (1U << 2U)  // Bit 2: Identifier Extension
+
+#define CAN_EXT_ID_MASK                 0x1FFFFFFFU
+#define CAN_STD_ID_MASK                 0x000007FFU
+
+/**
+   Decodes CAN messages from the data registers and populates a
+   CAN message struct with the data fields.
+
+   @preconditions     - A valid CAN message is received
+   @params CAN_rx_msg - CAN message structure for reception
+
+*/
+void CANReceive(CAN_msg_t* CAN_rx_msg)
+{
+  uint32_t id = CAN->sFIFOMailBox[0].RIR;
+  if ((id & STM32_CAN_RIR_IDE) == 0) { // Standard frame format
+    CAN_rx_msg->format = STANDARD_FORMAT;;
+    CAN_rx_msg->id = (CAN_STD_ID_MASK & (id >> 21U));
+  }
+  else {                               // Extended frame format
+    CAN_rx_msg->format = EXTENDED_FORMAT;;
+    CAN_rx_msg->id = (CAN_EXT_ID_MASK & (id >> 3U));
+  }
+
+  if ((id & STM32_CAN_RIR_RTR) == 0) { // Data frame
+    CAN_rx_msg->type = DATA_FRAME;
+  }
+  else {                               // Remote frame
+    CAN_rx_msg->type = REMOTE_FRAME;
+  }
+
+
+  CAN_rx_msg->len = (CAN->sFIFOMailBox[0].RDTR) & 0xFUL;
+
+  CAN_rx_msg->data[0] = 0xFFUL &  CAN->sFIFOMailBox[0].RDLR;
+  CAN_rx_msg->data[1] = 0xFFUL & (CAN->sFIFOMailBox[0].RDLR >> 8);
+  CAN_rx_msg->data[2] = 0xFFUL & (CAN->sFIFOMailBox[0].RDLR >> 16);
+  CAN_rx_msg->data[3] = 0xFFUL & (CAN->sFIFOMailBox[0].RDLR >> 24);
+  CAN_rx_msg->data[4] = 0xFFUL &  CAN->sFIFOMailBox[0].RDHR;
+  CAN_rx_msg->data[5] = 0xFFUL & (CAN->sFIFOMailBox[0].RDHR >> 8);
+  CAN_rx_msg->data[6] = 0xFFUL & (CAN->sFIFOMailBox[0].RDHR >> 16);
+  CAN_rx_msg->data[7] = 0xFFUL & (CAN->sFIFOMailBox[0].RDHR >> 24);
+
+  // Release FIFO 0 output mailbox.
+  // Make the next incoming message available.
+  CAN->RF0R |= 0x20UL;
+}
+
+/**
+   Encodes CAN messages using the CAN message struct and populates the
+   data registers with the sent.
+
+   @params CAN_tx_msg - CAN message structure for transmission
+
+*/
+void CANSend(CAN_msg_t* CAN_tx_msg)
+{
+  volatile int count = 0;
+
+  uint32_t out = 0;
+  if (CAN_tx_msg->format == EXTENDED_FORMAT) { // Extended frame format
+    out = ((CAN_tx_msg->id & CAN_EXT_ID_MASK) << 3U) | STM32_CAN_TIR_IDE;
+  }
+  else {                                  // Standard frame format
+    out = ((CAN_tx_msg->id & CAN_STD_ID_MASK) << 21U);
+  }
+
+  // Remote frame
+  if (CAN_tx_msg->type == REMOTE_FRAME) {
+    out |= STM32_CAN_TIR_RTR;
+  }
+
+  CAN->sTxMailBox[0].TDTR &= ~(0xF);
+  CAN->sTxMailBox[0].TDTR |= CAN_tx_msg->len & 0xFUL;
+
+  CAN->sTxMailBox[0].TDLR  = (((uint32_t) CAN_tx_msg->data[3] << 24) |
+                              ((uint32_t) CAN_tx_msg->data[2] << 16) |
+                              ((uint32_t) CAN_tx_msg->data[1] <<  8) |
+                              ((uint32_t) CAN_tx_msg->data[0]      ));
+  CAN->sTxMailBox[0].TDHR  = (((uint32_t) CAN_tx_msg->data[7] << 24) |
+                              ((uint32_t) CAN_tx_msg->data[6] << 16) |
+                              ((uint32_t) CAN_tx_msg->data[5] <<  8) |
+                              ((uint32_t) CAN_tx_msg->data[4]      ));
+
+  // Send Go
+  CAN->sTxMailBox[0].TIR = out | STM32_CAN_TIR_TXRQ;
+
+  // Wait until the mailbox is empty
+  while (CAN->sTxMailBox[0].TIR & 0x1UL && count++ < 100000);
+
+  // The mailbox don't becomes empty while loop
+  if (CAN->sTxMailBox[0].TIR & 0x1UL) {
+    Serial.println("Send Fail");
+    Serial.println(CAN->ESR);
+    Serial.println(CAN->MSR);
+    Serial.println(CAN->TSR);
+  }
+}
+
+/**
+  Returns whether there are CAN messages available.
+
+  @returns If pending CAN messages are in the CAN controller
+
+*/
+uint8_t CANMsgAvail(void)
+{
+  // Check for pending FIFO 0 messages
+  return CAN->RF0R & 0x3UL;
+}
+
+
+
+
+
+
+
+
+
 /***********************************************
- * Global Variables
+   Global Variables
  ***********************************************/
 
 /*** Frequency. ***/
@@ -26,15 +394,20 @@ bool isError = false;
 float Temperature_C = 0;
 unsigned int Temperature_F = 0;
 
+/*** canbus interval output ***/
+const int CAN_STBY_PORT = PA2;
+const int canInterval = 100; // in MS, transmit singal every 100MS
+static unsigned long canTime = 0;//time Keeper
+
 /************************************************
- * Main loop service procedures
+   Main loop service procedures
  ************************************************/
 
 /* Function table for flex fuel service functions. */
 flexFuelService_t flexFuelServiceFnTbl[FF_HDLR_TOTAL] = {NULL};
 
 /************************************************
- * Initialization functions
+   Initialization functions
  ************************************************/
 void registerServiceFns(void)
 {
@@ -48,24 +421,26 @@ void registerServiceFns(void)
   flexFuelServiceFnTbl[FF_HDLR_BT]               = serviceBluetooth;
 
   /* Debug features: Only used for debugging. */
-  #ifdef DEBUG
+#ifdef DEBUG
   initAndRegisterSerialTerminalDebug();
-  #endif
+#endif
 }
 
 /************************************************
- * Setup: software entry point.
+   Setup: software entry point.
  ************************************************/
 void setup(void)
 {
   SETUP_ISR();
   registerServiceFns();
+  CAN_INIT();
+
 
   /* Run loop(). */
 }
 
 /************************************************
- * Main service loop
+   Main service loop
  ************************************************/
 void loop(void)
 {
@@ -77,4 +452,34 @@ void loop(void)
       flexFuelServiceFnTbl[i]();
     }
   }
+
+  if ((millis() - canTime) > canInterval) {
+    sendCanMsg();
+    canTime = millis();
+  }
+}
+
+
+
+
+
+
+
+void CAN_INIT() {
+  bool ret = CANInit(CAN_500KBPS, 2);  // CAN_RX mapped to PB8, CAN_TX mapped to PB9
+  pinMode(CAN_STBY_PORT, OUTPUT);//set the canbus tranciver to active
+  digitalWrite(CAN_STBY_PORT, LOW);
+}
+
+void sendCanMsg() {
+  CAN_msg_t CAN_TX_msg;
+  CAN_TX_msg.len = 4;// 4 bytes total
+  CAN_TX_msg.type = DATA_FRAME;
+  CAN_TX_msg.format = STANDARD_FORMAT;
+  CAN_TX_msg.id = 0x600;// we set can ID to 0x600
+  CAN_TX_msg.data[0] = ethanol;
+  CAN_TX_msg.data[1] = Temperature_C;
+  CAN_TX_msg.data[2] = Temperature_F;
+  bitWrite(CAN_TX_msg.data[3] , 0, isError);// we set bit0 of byte4 to the error flag
+  CANSend(&CAN_TX_msg);
 }
